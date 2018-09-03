3a38b7fa8b8a54aab742cd15665d514ab8350d43
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-03 12:52:36 +0200

    added basic calculations with decimals

---
+++ calculate.js
@@ -0,0 +1,15 @@
+const calculate = (n1, opeator, n2) => {
+    let result = ''
+    
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'substract') {
+        result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2)
+    } else if (opeator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2)
+    }
+    
+    return result
+}
