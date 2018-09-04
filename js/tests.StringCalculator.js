bc806488fa9f1fb9ade11568cf66229195df6e29
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-04 14:01:07 +0200

    work on string calculator

--- js/tests.StringCalculator.js
+++ js/tests.StringCalculator.js
@@ -1,12 +1,17 @@
-function test_printing(a, b) {
-    if ((a.length == 1) || (b.length == 1) || (a.length == 2) || (b.length == 2)) {
+function test_printing(s) {
+    if (s.length == 0) {
+        return "ok, 0";
+    }
+    else if ((s.length == 1) || (s.length == 3)) {
         return "ok";
     }
     else {
-         return "fail, string is too short or too long";
+         return "fail, string is too long";
     }
 }
 
 function add(a, b) {
-    
+    valueA = parseInt(a);
+    valueB = parseInt(b);
+    return sum == valueA + valueB;
 }
