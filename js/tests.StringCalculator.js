044d7a05fbbd877d171b238817a6fab5f901f539
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-04 14:41:12 +0200

    work on string calculator

--- js/tests.StringCalculator.js
+++ js/tests.StringCalculator.js
@@ -1,6 +1,6 @@
 function test_printing(s) {
     if (s.length == 0) {
-        return "ok, 0";
+        return "ok, string length is 0";
     }
     else if ((s.length == 1) || (s.length == 3)) {
         return "ok";
@@ -10,8 +10,14 @@ function test_printing(s) {
     }
 }
 
-function add(a, b) {
-    valueA = parseInt(a);
-    valueB = parseInt(b);
-    return sum == valueA + valueB;
+function add(s) {
+    var nums = [];
+    nums.push(s.split(","));
+    sum = 0;
+    for (i = 0; i < nums.length; i++) {
+        sum+= parseInt(nums[i]);
+    }
+    return sum;
 }
+
+console.log(add("2,2"));
