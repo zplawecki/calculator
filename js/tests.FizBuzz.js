35a59376cbe32401c085f5f453f284b208b85148
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-04 13:40:33 +0200

    fixed displaying numbers divided by 3 and by 5

--- js/tests.FizBuzz.js
+++ js/tests.FizBuzz.js
@@ -8,9 +8,9 @@ function test_printing(result, expected) {
 }
 
 function print(a) {
-    if (a % 3 == 0)  {
+    if ((a % 3 == 0) && (a % 5 != 0))  {
         return "Fizz";
-    }  else if (a % 5 == 0) {
+    }  else if ((a % 5 == 0) && (a % 3 != 0)){
         return "Buzz";
     } else if ((a % 3 == 0) && (a % 5 == 0)) {
         return "FizzBuzz";
@@ -19,8 +19,13 @@ function print(a) {
     }
 }
 
-var a;
-for (a = 1; a <= 100; a++) {
-    console.log(print(a));
-    test_printing(print(a), "FizzBuzz");
-}
+// var a;
+// for (a = 1; a <= 100; a++) {
+//     console.log(print(a));
+//     test_printing(print(a), "FizzBuzz");
+// }
+
+test_printing(print(1), "1");
+test_printing(print(3), "Fizz");
+test_printing(print(5), "Buzz");
+test_printing(print(15), "FizzBuzz");
---
+++ js/tests.StringCalculator.js
@@ -0,0 +1,12 @@
+function test_printing(a, b) {
+    if ((a.length == 1) || (b.length == 1) || (a.length == 2) || (b.length == 2)) {
+        return "ok";
+    }
+    else {
+         return "fail, string is too short or too long";
+    }
+}
+
+function add(a, b) {
+    
+}
