d5bfa167c5f7907205748f51e4019e17e5289371
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-09 23:25:44 +0200

    fixed error for showing results of methods, added comments

--- js/tests.StringCalculator.js
+++ js/tests.StringCalculator.js
@@ -12,13 +12,17 @@ function test_string(s) {
 
 function add(s) {
     const numbers = s
+    //split string into numbers divided by ,
         .split(',')
+    //create an array where every element is parsed from string to int in decimal system
         .map(n => parseInt(n, 10));
-    
+    //check if any element of array passes test (is illegal number or negative number)
     if(numbers.some(n => Number.isNaN(n))) throw new Error('Not a number');
     if(numbers.some(n => n < 0)) throw new Error('Negatives not allowed');
-
+    //reduces the array to single value, here - the sum of elements
     return numbers.reduce((s, n) => s + n);
 }
 
-add("2,2");
+s = "2,5";
+console.log(test_string(s));
+console.log(add(s));
