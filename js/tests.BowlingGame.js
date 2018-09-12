32a35beb3530914a470061f8697f3d50c7e3acf5
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-12 12:48:28 +0200

    attempt to ask for input and display it in console

--- js/tests.BowlingGame.js
+++ js/tests.BowlingGame.js
@@ -1,22 +1,36 @@
-function test_game(score) {
-    if (score < 130) {
-        return "ok, score is correct";
-    }
-    else if (score == 130) {
-        return "ok, score is perfect";
-    }
-    else {
-         return "fail, score is incorrect";
-    }
+// function test_game(score) {
+//     if (score < 130) {
+//         return "ok, score is correct";
+//     }
+//     else if (score == 130) {
+//         return "ok, score is perfect";
+//     }
+//     else {
+//          return "fail, score is incorrect";
+//     }
+// }
+
+// function score(s) {
+//     const numbers = s
+//         .split(',')
+//         .map(n => parseInt(n, 10));
+//     return numbers.reduce((s, n) => s + n);
+// }
+
+function enterPlayerName() {
+    var name = prompt("Podaj imie: ", "");
+    console.log("name");
+    return name;
 }
 
-function score(s) {
-    const numbers = s
-        .split(',')
-        .map(n => parseInt(n, 10));
-    return numbers.reduce((s, n) => s + n);
-}
+enterPlayerName();
 
-s = "10,10,10,10,10,10,10,10,10,10";
-console.log(test_game(s));
-console.log(score(s));
+// function enterFrameScore() {
+//     for (i = 0; i < 10; i++) {
+//     console.log("Podaj wynik frame nr: " + i);
+//     }
+// }
+
+// s = "10,10,10,10,10,10,10,10,10,10,10,10,10";
+// console.log(test_game(score(s)));
+// console.log(score(s));
