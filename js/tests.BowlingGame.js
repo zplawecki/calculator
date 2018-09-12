fc07167640832ed1397d808ec1431cc3a98f1bfd
Zbigniew Pławecki <zbigniew.plawecki@chanda.pl>
2018-09-12 13:52:13 +0200

    work on providing frame score

--- js/tests.BowlingGame.js
+++ js/tests.BowlingGame.js
@@ -1,36 +1,49 @@
-// function test_game(score) {
-//     if (score < 130) {
-//         return "ok, score is correct";
-//     }
-//     else if (score == 130) {
-//         return "ok, score is perfect";
-//     }
-//     else {
-//          return "fail, score is incorrect";
-//     }
-// }
-
-// function score(s) {
-//     const numbers = s
-//         .split(',')
-//         .map(n => parseInt(n, 10));
-//     return numbers.reduce((s, n) => s + n);
-// }
-
-function enterPlayerName() {
-    var name = prompt("Podaj imie: ", "");
-    console.log("name");
-    return name;
+function test_game(score) {
+    if (score < 130) {
+        return "ok, score is correct";
+    }
+    else if (score == 130) {
+        return "ok, score is perfect";
+    }
+    else {
+         return "fail, score is incorrect";
+    }
 }
 
-enterPlayerName();
+function enterNameAndScore() {
+    var Input = require('prompt-input');
+    var score = [];
+    var name = new Input({
+        name: "name",
+        message: "What is your name?"
+    });
 
-// function enterFrameScore() {
-//     for (i = 0; i < 10; i++) {
-//     console.log("Podaj wynik frame nr: " + i);
-//     }
-// }
+    name.ask(function(answers) {
+        console.log("Hello " + answers);
+    });
 
+    for (i = 0; i < 10; i++) {
+        var scores = new Input({
+            name: "score",
+            message: "Podaj wynik ramki nr: " + i
+        });
+    
+        scores.ask(function(answers) {
+            score.push(answers);
+        });
+    }
+
+}
+
+function score(s) {
+    const score = s
+        .split(',')
+        .map(n => parseInt(n, 10));
+    return score.reduce((s, n) => s + n);
+}
+
+enterNameAndScore();
 // s = "10,10,10,10,10,10,10,10,10,10,10,10,10";
 // console.log(test_game(score(s)));
 // console.log(score(s));
+
